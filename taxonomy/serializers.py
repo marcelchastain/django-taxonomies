67b8d7d0ad453babb3e1183fc38587bf6d9d3756
Marcel Chastain <marcel.chastain@gmail.com>
2014-09-01 16:12:52 -0700

    added counts

--- taxonomy/serializers.py
+++ taxonomy/serializers.py
@@ -3,11 +3,21 @@ from .models import (TaxonomyGroup, TaxonomyItem)
 
 
 class TaxonomyGroupSerializer(serializers.ModelSerializer):
+    item_count = serializers.SerializerMethodField('get_item_count')
+
     class Meta:
         model = TaxonomyGroup
         fields = ('id', 'name')
 
+    def get_item_count(self, obj):
+        return obj.taxonomyitem_set.count()
+
 class TaxonomyItemSerializer(serializers.ModelSerializer):
+    map_count = serializers.SerializerMethodField('get_map_count')
+
     class Meta:
         model = TaxonomyItem
         fields = ('id', 'taxonomy_group', 'parent', 'name')
+
+    def get_map_count(self, obj):
+        return obj.taxonomymap_set.count()
