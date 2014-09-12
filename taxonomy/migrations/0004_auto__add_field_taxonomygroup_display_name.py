d10af3bb06f6d3b474cfd41a6161fa93072d09c2
Marcel Chastain <marcel.chastain@gmail.com>
2014-09-12 15:59:46 -0700

    added display_name

---
+++ taxonomy/migrations/0004_auto__add_field_taxonomygroup_display_name.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'TaxonomyGroup.display_name'
+        db.add_column(u'taxonomy_taxonomygroup', 'display_name',
+                      self.gf('django.db.models.fields.CharField')(max_length=150, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'TaxonomyGroup.display_name'
+        db.delete_column(u'taxonomy_taxonomygroup', 'display_name')
+
+
+    models = {
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'taxonomy.taxonomygroup': {
+            'Meta': {'ordering': "['display_name']", 'object_name': 'TaxonomyGroup'},
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '150', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '75', 'db_index': 'True'})
+        },
+        u'taxonomy.taxonomyitem': {
+            'Meta': {'ordering': "['name']", 'object_name': 'TaxonomyItem'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '75', 'db_index': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['taxonomy.TaxonomyItem']", 'null': 'True', 'blank': 'True'}),
+            'taxonomy_group': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['taxonomy.TaxonomyGroup']"})
+        },
+        u'taxonomy.taxonomymap': {
+            'Meta': {'object_name': 'TaxonomyMap'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'taxonomy_item': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['taxonomy.TaxonomyItem']"})
+        }
+    }
+
+    complete_apps = ['taxonomy']
